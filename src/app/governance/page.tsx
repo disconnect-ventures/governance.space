85fc9e4cb07103c58c2c3b044185e674e3cb01c3
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-14 13:57:31 -0300

    chore: Governament action directory structure sketch

---
+++ src/app/governance/page.tsx
@@ -0,0 +1,5 @@
+// https://www.figma.com/design/bfEklPIeZVRf0P6xC6f1e7/Governance-Space?node-id=34-1870&t=GGJEhGlKd8rVords-4
+
+export default function GovernancePage() {
+  return <div className="space-y-4">TODO</div>;
+}
