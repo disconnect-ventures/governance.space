7b77b2899083cf045f5e7b12a814dc47fc7b8082
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-15 20:05:37 -0300

    feat: Tweak VotingHistory styles and add separator component

--- src/components/features/profile/VotingHistory.tsx
+++ src/components/features/profile/VotingHistory.tsx
@@ -1,9 +1,10 @@
 // https://www.figma.com/design/bfEklPIeZVRf0P6xC6f1e7/Governance-Space?node-id=50-19870&t=GGJEhGlKd8rVords-4
 import { Calendar, CircleX, CircleMinus, CircleCheck } from "lucide-react";
-import { Card, CardContent } from "~/components/ui/card";
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import { Badge } from "~/components/ui/badge";
 import { Proposal } from "~/lib/proposals";
 import clsx from "clsx";
+import { Separator } from "~/components/ui/separator";
 
 type VotingHistoryProps = {
   proposals: Array<Proposal>;
@@ -61,62 +62,68 @@ export const VotingHistory = ({ proposals }: VotingHistoryProps) => {
   };
 
   return (
-    <div className="w-full mx-auto">
-      <h1 className="text-2xl font-bold mb-6">Voting History</h1>
-      <div className="space-y-4">
-        {proposals.map((proposal) => {
+    <Card>
+      <CardHeader className="text-xl font-semibold">Voting History</CardHeader>
+      <CardContent className="space-y-4">
+        {proposals.map((proposal, index) => {
           const vote = getUserVote();
 
           return (
-            <Card key={proposal.id} className="w-full">
-              <CardContent className="p-6">
-                <div className="flex flex-col space-y-4">
-                  <div className="flex flex-col md:flex-row md:items-start md:justify-between gap-4">
-                    <div className="flex-1">
-                      <div className="flex gap-4 md:gap-8 mb-2 justify-between">
-                        <div className="flex flex-col md:flex-row gap-2 md:items-center">
-                          <h2 className="text-lg font-semibold">
-                            {proposal.attributes.content.attributes.prop_name}
-                          </h2>
-                          <Badge
-                            variant="secondary"
-                            className={clsx(
-                              proposal.attributes.content.attributes
-                                .prop_submitted
-                                ? "bg-green-100 text-green-800"
-                                : "bg-yellow-100 text-yellow-800",
-                              "w-fit mr-auto"
-                            )}
-                          >
-                            {getSubmissionStatus(proposal)}
-                          </Badge>
+            <>
+              {index > 0 && <Separator />}
+              <Card
+                key={proposal.id}
+                className="w-full shadow-none border-none"
+              >
+                <CardContent className="px-0">
+                  <div className="flex flex-col space-y-4">
+                    <div className="flex flex-col md:flex-row md:items-start md:justify-between gap-4">
+                      <div className="flex-1">
+                        <div className="flex gap-4 md:gap-8 mb-2 justify-between">
+                          <div className="flex flex-col md:flex-row gap-2 md:items-center">
+                            <h2 className="text-lg font-semibold">
+                              {proposal.attributes.content.attributes.prop_name}
+                            </h2>
+                            <Badge
+                              variant="secondary"
+                              className={clsx(
+                                proposal.attributes.content.attributes
+                                  .prop_submitted
+                                  ? "bg-green-100 text-green-800"
+                                  : "bg-yellow-100 text-yellow-800",
+                                "w-fit mr-auto"
+                              )}
+                            >
+                              {getSubmissionStatus(proposal)}
+                            </Badge>
+                          </div>
+                          <div className="">{getVoteBadge(vote)}</div>
                         </div>
-                        <div className="">{getVoteBadge(vote)}</div>
-                      </div>
-                      <p className="text-gray-600 mb-2">
-                        {proposal.attributes.content.attributes.prop_abstract}
-                      </p>
-                      <div className="flex flex-wrap items-center gap-2 text-gray-500 text-sm">
-                        <Calendar className="w-4 h-4" />
-                        <span>
-                          {formatDate(
-                            proposal.attributes.content.attributes.createdAt
-                          )}
-                        </span>
-                        <span className="text-gray-400">|</span>
-                        <span>
-                          ID:{" "}
-                          {proposal.attributes.content.attributes.proposal_id}
-                        </span>
+                        <p className="text-gray-600 mb-2">
+                          {proposal.attributes.content.attributes.prop_abstract}
+                        </p>
+                        <div className="flex flex-wrap items-center gap-2 text-gray-500 text-sm">
+                          <Calendar className="w-4 h-4" />
+                          <span>
+                            {formatDate(
+                              proposal.attributes.content.attributes.createdAt
+                            )}
+                          </span>
+                          <span className="text-gray-400">|</span>
+                          <span>
+                            ID:{" "}
+                            {proposal.attributes.content.attributes.proposal_id}
+                          </span>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </CardContent>
-            </Card>
+                </CardContent>
+              </Card>
+            </>
           );
         })}
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   );
 };
