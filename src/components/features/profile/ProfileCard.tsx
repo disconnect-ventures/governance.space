623739cf4f392beec9fe3bcaccf0ef3388cdf4cd
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-16 14:30:27 -0300

    feat: Align ProfileCard left on smaller devices

--- src/components/features/profile/ProfileCard.tsx
+++ src/components/features/profile/ProfileCard.tsx
@@ -15,31 +15,35 @@ export function ProfileCard({ drep }: ProfileCardProps) {
   return (
     <Card className="w-full bg-white shadow-sm space-y-2">
       <CardHeader className="pb-4">
-        <div className="flex flex-col md:flex-row items-center gap-4">
-          <div className="relative w-12 h-12">
-            <Avatar className="h-16 w-16">
-              <AvatarImage src={drep.imageUrl ?? ""} />
-              <AvatarFallback>{drep.givenName?.substring(0, 2)}</AvatarFallback>
-            </Avatar>
-          </div>
-
-          <div className="flex-1 flex flex-col md:flex-row gap-2">
-            <div className="flex items-center gap-2">
-              <h2 className="text-xl font-bold">{drep.givenName}</h2>
-              <span className="text-yellow-400">
-                <Star></Star>
-              </span>
+        <div className="flex flex-col md:flex-row md:items-center gap-4">
+          <div className="flex gap-8 py-4">
+            <div className="relative w-12 h-12">
+              <Avatar className="h-16 w-16">
+                <AvatarImage src={drep.imageUrl ?? ""} />
+                <AvatarFallback>
+                  {drep.givenName?.substring(0, 2)}
+                </AvatarFallback>
+              </Avatar>
             </div>
 
-            <Badge
-              variant="secondary"
-              className="w-fit mx-auto md:mx-0 bg-green-100 text-green-800 hover:bg-green-100 rounded-full"
-            >
-              {drep.status}
-            </Badge>
+            <div className="flex-1 flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <h2 className="text-xl font-bold">{drep.givenName}</h2>
+                <span className="text-yellow-400">
+                  <Star></Star>
+                </span>
+              </div>
+
+              <Badge
+                variant="secondary"
+                className="w-fit bg-green-100 text-green-800 hover:bg-green-100 rounded-full"
+              >
+                {drep.status}
+              </Badge>
+            </div>
           </div>
 
-          <div className="flex flex-wrap gap-2 justify-center md:justify-end">
+          <div className="flex flex-wrap gap-2 justify-start md:justify-end md:ml-auto">
             <Button variant="outline">Send message</Button>
             <Button className="bg-blue-600 hover:bg-blue-700">
               <ArrowRightCircle />
@@ -50,7 +54,7 @@ export function ProfileCard({ drep }: ProfileCardProps) {
       </CardHeader>
 
       <CardContent>
-        <div className="w-full flex flex-col md:flex-row items-center gap-2 text-sm text-gray-600 overflow-hidden">
+        <div className="w-full flex flex-row items-center gap-2 text-sm text-gray-600 overflow-hidden">
           <span>DRep ID:</span>
           <span className="w-full sm:w-fit text-ellipsis overflow-hidden">
             {drep.drepId}
