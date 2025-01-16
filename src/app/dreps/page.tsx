3a3ef538e5975f958811ccc275b148fdc7965c14
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-16 17:01:02 -0300

    feat: Extracts part of page header into PageTitle component and adapt current pages

--- src/app/dreps/page.tsx
+++ src/app/dreps/page.tsx
@@ -1,9 +1,10 @@
 // https://www.figma.com/design/bfEklPIeZVRf0P6xC6f1e7/Governance-Space?node-id=30-346&t=GGJEhGlKd8rVords-4
-import { Users } from "lucide-react";
+import { UsersIcon } from "lucide-react";
 import {
   DRepsDirectory,
   DRepsDirectoryProps,
 } from "~/components/features/dreps/DRepsDirectory";
+import { PageTitle } from "~/components/layout/PageTitle";
 import { Badge } from "~/components/ui/badge";
 import { getDReps } from "~/lib/dreps";
 
@@ -11,20 +12,14 @@ export default async function DRepsDirectoryPage() {
   const dreps = await getDReps();
   return (
     <div className="space-y-4">
-      <div className="flex items-center justify-between mb-6">
-        <div className="flex items-center gap-4">
-          <div className="p-2 rounded-full bg-gray-300">
-            <Users className="h-6 w-6" />
-          </div>
-          <h1 className="text-4xl font-bold">DReps Directory</h1>
-          <Badge
-            variant="secondary"
-            className="bg-gray-100 border-primary p-2 rounded-full"
-          >
-            259 registered DReps
-          </Badge>
-        </div>
-      </div>
+      <PageTitle title={"DReps Directory"} icon={<UsersIcon />}>
+        <Badge
+          variant="secondary"
+          className="bg-gray-100 border-foreground p-2 rounded-full"
+        >
+          259 registered DReps
+        </Badge>
+      </PageTitle>
       <DRepsDirectory dreps={dreps as DRepsDirectoryProps["dreps"]} />
     </div>
   );
