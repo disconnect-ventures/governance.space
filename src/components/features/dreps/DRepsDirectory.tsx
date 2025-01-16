c588ed4a9f2350a762c7c9203ddf5eb8113e741d
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-16 17:38:07 -0300

    feat: Add breakpoints to directory searches

--- src/components/features/dreps/DRepsDirectory.tsx
+++ src/components/features/dreps/DRepsDirectory.tsx
@@ -75,7 +75,7 @@ export const DRepsDirectory = ({ dreps }: DRepsDirectoryProps) => {
   return (
     <Card className="w-full mx-auto shadow-none border-none bg-gray-100">
       <CardContent className="p-0">
-        <div className="flex items-center gap-4 mb-6">
+        <div className="flex flex-col sm:flex-row sm:items-center gap-x-4 gap-y-2 mb-6">
           <div className="flex-1">
             <Input
               placeholder="Search DReps by name or ID"
