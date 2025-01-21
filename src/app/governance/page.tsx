fe468d7262e5449a13ac703a4343a5cc0d0d0c40
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-21 19:14:22 -0300

    chore: Update governance actions page icon

--- src/app/governance/page.tsx
+++ src/app/governance/page.tsx
@@ -1,8 +1,7 @@
 // https://www.figma.com/design/bfEklPIeZVRf0P6xC6f1e7/Governance-Space?node-id=34-1870&t=GGJEhGlKd8rVords-4
-import { FileTextIcon, HandHelpingIcon } from "lucide-react";
+import { BookOpenCheckIcon } from "lucide-react";
 import { GovernanceActionDirectory } from "~/components/features/governance/GovernanceActionDirectory";
 import { PageTitle } from "~/components/layout/PageTitle";
-import { Badge } from "~/components/ui/badge";
 import { getGovernanceActions } from "~/lib/governance-actions";
 
 export default async function GovernancePage() {
@@ -15,18 +14,10 @@ export default async function GovernancePage() {
         title="Governance Actions"
         icon={
           <div className="p-2 rounded-full bg-gray-300 w-12 h-12 flex flex-col justify-center items-center">
-            <FileTextIcon className="w-5 h-5 relative top-1" />
-            <HandHelpingIcon className="w-6 h-6" />
+            <BookOpenCheckIcon />
           </div>
         }
-      >
-        <Badge
-          variant="secondary"
-          className="bg-gray-100 border-foreground p-2 rounded-full"
-        >
-          12 registered proposals
-        </Badge>
-      </PageTitle>
+      ></PageTitle>
       <GovernanceActionDirectory governanceActions={governanceActions} />
     </div>
   );
