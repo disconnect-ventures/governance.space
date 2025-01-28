5f57cd0d100c33f490e918e8e9732668a17dde05
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-28 16:07:35 -0300

    feat: Fix logo with correct icon

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Logo() {
   return (
     <div className="w-auto h-full hover:text-accent">
       <Link className="w-full" href="/" aria-label="Home">
-        <div className="flex items-center">
-          <span className="text-xl font-semibold">🏛️ GOVERNANCE</span>
+        <div className="flex items-center whitespace-nowrap focus:text-inherit">
+          <Image
+            src={"/icon-512.png"}
+            height={512}
+            width={512}
+            alt="logo-icon"
+            className="h-10 w-10"
+          />
+          <span className="text-xl font-semibold text-gray-800">
+            GOVERNANCE
+          </span>
           <span className="text-xl text-gray-500">SPACE</span>
         </div>
       </Link>
