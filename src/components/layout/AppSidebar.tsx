7f4d257cc87e20f06be44641a30163d08d74398b
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-28 17:58:45 -0300

    feat: Increase AppSidebar content gap

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -23,7 +23,7 @@ export function AppSidebar() {
     <Sidebar className=" md:hidden">
       <SidebarContent className="pb-8">
         <SidebarGroup className="h-full">
-          <SidebarGroupContent className="h-full flex flex-col gap-2">
+          <SidebarGroupContent className="h-full flex flex-col gap-4">
             <div className="relative max-w-sm">
               <Input type="text" placeholder="Search..." className="pl-8" />
               <SearchIcon className="absolute left-2 top-2.5 h-4 w-4 text-gray-500" />
