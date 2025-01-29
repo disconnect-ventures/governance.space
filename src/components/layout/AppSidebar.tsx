3516535f21c3cfbdbe67b5e136dd8e82fb6ec503
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-29 16:54:14 -0300

    fix: Fixes sidebar blank container showing up on desktop

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -20,38 +20,40 @@ export function AppSidebar() {
   const pathname = usePathname();
 
   return (
-    <Sidebar className=" md:hidden">
-      <SidebarContent className="pb-8">
-        <SidebarGroup className="h-full">
-          <SidebarGroupContent className="h-full flex flex-col gap-4">
-            <div className="relative max-w-sm">
-              <Input type="text" placeholder="Search..." className="pl-8" />
-              <SearchIcon className="absolute left-2 top-2.5 h-4 w-4 text-gray-500" />
-            </div>
-            <SidebarMenu>
-              {headerNavLinks.map((item, index) => (
-                <SidebarMenuItem key={index}>
-                  <SidebarMenuButton asChild>
-                    <Link
-                      href={item.href}
-                      className={clsx(
-                        pathname === item.href &&
-                          "bg-gray-800 text-gray-200 focus:bg-gray-800 focus:text-gray-200"
-                      )}
-                    >
-                      <span>{item.label}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-            <div className="flex flex-col gap-2 mt-auto">
-              <Button variant="ghost">Become a DRep</Button>
-              <Button>Connect Wallet</Button>
-            </div>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
+    <div className="md:hidden">
+      <Sidebar>
+        <SidebarContent className="pb-8">
+          <SidebarGroup className="h-full">
+            <SidebarGroupContent className="h-full flex flex-col gap-4">
+              <div className="relative max-w-sm">
+                <Input type="text" placeholder="Search..." className="pl-8" />
+                <SearchIcon className="absolute left-2 top-2.5 h-4 w-4 text-gray-500" />
+              </div>
+              <SidebarMenu>
+                {headerNavLinks.map((item, index) => (
+                  <SidebarMenuItem key={index}>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        href={item.href}
+                        className={clsx(
+                          pathname === item.href &&
+                            "bg-gray-800 text-gray-200 focus:bg-gray-800 focus:text-gray-200"
+                        )}
+                      >
+                        <span>{item.label}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+              <div className="flex flex-col gap-2 mt-auto">
+                <Button variant="ghost">Become a DRep</Button>
+                <Button>Connect Wallet</Button>
+              </div>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    </div>
   );
 }
