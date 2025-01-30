1e6fd7ba74baca8f71eed06020d5d06c2e05228a
Otávio Lima <contato@otaviolima.com>
2025-01-30 16:58:38 -0300

    Update text and color AnnouncementBar

--- src/components/layout/AnnouncementBar.tsx
+++ src/components/layout/AnnouncementBar.tsx
@@ -2,12 +2,12 @@ import { AlertCircleIcon } from "lucide-react";
 
 export function AnnouncementBar() {
   return (
-    <div className="bg-gray-200 text-center px-2 md:px-4 py-1">
+    <div className="bg-blue-200 text-center px-2 md:px-4 py-1">
       <div className="w-fit"></div>
       <span className="text-sm">
         <AlertCircleIcon className="h-4 w-4 text-yellow-600 inline mr-2" />
-        This site is under development and will officially launch on January
-        29th at 21:30 UTC
+        This site is under development and will officially launch on February
+        3rd at 21:30 UTC at Epoch 536.
       </span>
     </div>
   );
