929abc5939c64da1f26378dc710ca672926ef91d
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-31 12:00:15 -0300

    fix: Make info box take full width

--- src/components/layout/PageTitle.tsx
+++ src/components/layout/PageTitle.tsx
@@ -19,7 +19,7 @@ export function PageTitle({
 }: PageTitleProps) {
   return (
     <div className="flex items-center justify-between mb-6">
-      <div className="flex flex-wrap items-center gap-4">
+      <div className="flex flex-wrap items-center gap-4 w-full">
         <div className="p-2 rounded-full bg-gray-300 h-12 w-12 flex items-center justify-center">
           {icon}
         </div>
