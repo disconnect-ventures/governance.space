5e8a87839e8608ec83b833e376c6038293cf811a
Danilo Arantes F. <dfilho@purdue.edu>
2025-02-14 14:18:48 -0300

    chore: Remove locale from breadcrumbs

--- src/components/layout/Breadcrumbs.tsx
+++ src/components/layout/Breadcrumbs.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Link from "~/components/features/Link";
 import { usePathname } from "next/navigation";
 import { ChevronRight } from "lucide-react";
+import { i18n } from "~/config/i18n";
 
 interface BreadcrumbItem {
   label: string;
@@ -16,7 +17,9 @@ export const Breadcrumbs = () => {
   }
 
   const generateBreadcrumbs = (): BreadcrumbItem[] => {
-    const paths = pathname.split("/").filter(Boolean);
+    const paths = pathname
+      .split("/")
+      .filter((p) => Boolean(p) && !i18n.locales.some((l) => l.key === p));
 
     return paths.map((path, index) => {
       const href = "/" + paths.slice(0, index + 1).join("/");
