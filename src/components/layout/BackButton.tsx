8b2497eb49dc963642e8777d1bfc6d40cc309667
Danilo Arantes F. <dfilho@purdue.edu>
2025-02-14 14:10:10 -0300

    feat: Add locale to `router.push()` calls

--- src/components/layout/BackButton.tsx
+++ src/components/layout/BackButton.tsx
@@ -3,15 +3,20 @@
 import { ArrowLeft } from "lucide-react";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
+import { useLocale } from "~/hooks/use-locale";
+import { localizePath } from "~/lib/utils";
 
 type BackButtonProps = {
   href?: string;
 };
 export const BackButton = ({ href }: BackButtonProps) => {
   const router = useRouter();
+  const { locale } = useLocale();
   return (
     <Button
-      onClick={() => (href ? router.push(href) : router.back())}
+      onClick={() =>
+        href ? router.push(localizePath(locale, href)) : router.back()
+      }
       className="flex items-center text-blue-600"
       variant={"ghost"}
     >
