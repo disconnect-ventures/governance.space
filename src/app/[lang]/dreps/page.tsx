08bc59b306cfc9840aa6d9d5c21bdc61848d0ffa
Danilo Arantes F. <dfilho@purdue.edu>
2025-02-15 15:58:21 -0300

    feat: Replace more english defaults on Directory component and DReps page

--- src/app/[lang]/dreps/page.tsx
+++ src/app/[lang]/dreps/page.tsx
@@ -4,6 +4,7 @@ import { DRepsDirectory } from "~/components/features/dreps/DRepsDirectory";
 import { PageTitle } from "~/components/layout/PageTitle";
 import { DRepFilterOption, DRepSortOption, getDReps } from "~/lib/dreps";
 import { PageProps } from "../layout";
+import { getDictionary } from "~/config/dictionaries";
 
 export const metadata: Metadata = {
   title: "Governance Space - DReps Directory",
@@ -11,8 +12,10 @@ export const metadata: Metadata = {
 };
 
 export default async function DRepsDirectoryPage({
+  params: paramsPromise,
   searchParams: searchParamsPromise,
 }: PageProps) {
+  const params = await paramsPromise;
   const searchParams = (await searchParamsPromise) ?? {};
   const page = parseInt(searchParams["page"] ?? "0");
   const pageSize = parseInt(searchParams["pageSize"] ?? "20");
@@ -22,13 +25,16 @@ export default async function DRepsDirectoryPage({
   const search = searchParams["search"] ?? "";
   const dreps = await getDReps(page, pageSize, search, sort, filters);
   const { total: totalDReps } = await getDReps(0, 1, "", "Random", []);
+  const locale = params.lang;
+  const dictionary = await getDictionary(locale);
+  const badgeText = dictionary.pageDReps.badgeText;
 
   return (
     <div className="space-y-4">
       <PageTitle
         title={"DReps Directory"}
         icon={<UsersIcon />}
-        badge={`${totalDReps} registered DReps`}
+        badge={`${totalDReps} ${badgeText}`}
         translationPage="pageDReps"
       ></PageTitle>
       <DRepsDirectory
