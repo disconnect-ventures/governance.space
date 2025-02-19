e6525f42a43886bfcc01713f2cfc130a9be8682a
Danilo Arantes F. <dfilho@purdue.edu>
2025-02-19 15:13:28 -0300

    feat: Add vote counts and dates for governance action details

--- src/components/features/governance/GovernanceVoting.tsx
+++ src/components/features/governance/GovernanceVoting.tsx
@@ -1,42 +1,56 @@
-import { Calendar, Clock, Copy } from "lucide-react";
+import { Calendar, Clock } from "lucide-react";
 import {
-  GovernanceSectionVoting,
-  GovernanceSectionVotingProps,
-} from "./GovernanceSectionVoting";
+  GovernanceVotingCard,
+  GovernanceVotingCardProps,
+} from "./GovernanceVotingCard";
 import { Progress } from "~/components/ui/progress";
 import { GovernanceAction } from "~/lib/governance-actions";
-import { formatDate } from "~/lib/utils";
+import {
+  formatAda,
+  formatDate,
+  formatVotingPower,
+  truncateMiddle,
+} from "~/lib/utils";
+import CopyToClipboard from "../CopyToClipboard";
 
 export const GovernaceVoting = ({ action }: { action: GovernanceAction }) => {
-  const sposVotingData: GovernanceSectionVotingProps = {
+  // TODO:Get correct threshold
+  const sposVotingData: GovernanceVotingCardProps = {
     title: "SPOS",
-    votes: [
-      { type: "yes", amount: 298123958.303 },
-      { type: "no", amount: 107487.105 },
-      { type: "abstain", amount: 4124.547 },
-    ],
+    votes: {
+      yes: { count: action.poolYesVotes },
+      no: { count: action.poolNoVotes },
+      abstain: { count: action.poolAbstainVotes },
+    },
     threshold: 0.51,
   };
 
-  const constitutionalCommitteeVotingData: GovernanceSectionVotingProps = {
+  const constitutionalCommitteeVotingData: GovernanceVotingCardProps = {
     title: "Constitutional Committee",
-    votes: [
-      { type: "yes", amount: 164543736.097 },
-      { type: "no", amount: 98254.094 },
-      { type: "abstain", amount: 1453.986 },
-    ],
+    votes: {
+      yes: { count: action.ccYesVotes },
+      no: { count: action.ccNoVotes },
+      abstain: { count: action.ccAbstainVotes },
+    },
     threshold: 0.51,
   };
 
-  const dRepsVotingData: GovernanceSectionVotingProps = {
+  const dRepsVotingData: GovernanceVotingCardProps = {
     title: "DReps",
-    votes: [
-      { type: "yes", amount: 154937453.163 },
-      { type: "no", amount: 175986.543 },
-      { type: "abstain", amount: 2098.123 },
-    ],
+    votes: {
+      yes: { count: action.dRepYesVotes },
+      no: { count: action.dRepNoVotes },
+      abstain: { count: action.dRepAbstainVotes },
+    },
     threshold: 0.51,
+    formatValue: (value) => formatAda(formatVotingPower(value)),
   };
+
+  const totalDuration =
+    new Date(action.expiryDate).getTime() -
+    new Date(action.createdDate).getTime();
+  const currentProgress = Date.now() - new Date(action.createdDate).getTime();
+  const progressPercentage = (currentProgress / totalDuration) * 100;
 
   return (
     <div className="m-8 bg-white">
@@ -45,9 +59,9 @@ export const GovernaceVoting = ({ action }: { action: GovernanceAction }) => {
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-        <GovernanceSectionVoting {...sposVotingData} />
-        <GovernanceSectionVoting {...constitutionalCommitteeVotingData} />
-        <GovernanceSectionVoting {...dRepsVotingData} />
+        <GovernanceVotingCard {...dRepsVotingData} />
+        <GovernanceVotingCard {...sposVotingData} />
+        <GovernanceVotingCard {...constitutionalCommitteeVotingData} />
       </div>
 
       <div className="w-full flex flex-col md:flex-row gap-8 mt-6">
@@ -56,11 +70,9 @@ export const GovernaceVoting = ({ action }: { action: GovernanceAction }) => {
 
           <div className="flex items-center gap-2 mb-8">
             <code className="text-xs sm:text-sm font-mono break-all">
-              cef1e0b5b464244c0a285e659d65fe1d5159855866afde3b6259b88ff0#0
+              {truncateMiddle(action.txHash, 25)}
             </code>
-            <button className="p-2 hover:bg-gray-100 rounded flex-shrink-0">
-              <Copy className="h-4 w-4 text-gray-500" />
-            </button>
+            <CopyToClipboard value={action.txHash} />
           </div>
 
           <div className="space-y-3">
@@ -83,25 +95,18 @@ export const GovernaceVoting = ({ action }: { action: GovernanceAction }) => {
         </div>
 
         <div className="w-full md:flex-1">
-          <p className="mb-4 font-bold text-gray-600">
-            (CIP-129) Governance Action ID:
-          </p>
-
-          <div className="flex items-center gap-2 mb-8">
-            <code className="text-xs sm:text-sm font-mono break-all">
-              gov_action1emc7e07r4g4np2cvl3dge40mcakykdc3lcqqrfz8ce#0
-            </code>
-            <button className="p-2 hover:bg-gray-100 rounded flex-shrink-0">
-              <Copy className="h-4 w-4 text-gray-500" />
-            </button>
-          </div>
-
           <div className="space-y-2">
             <div className="flex justify-between">
               <p className="text-gray-600">Deadline</p>
-              <span className="font-medium text-[#2563EB]">75 %</span>
+              <span className="font-medium text-[#2563EB]">
+                {progressPercentage.toFixed(2)} %
+              </span>
             </div>
-            <Progress value={75} className="h-2" color="#2563EB" />
+            <Progress
+              value={progressPercentage}
+              className="h-2"
+              color="#2563EB"
+            />
           </div>
         </div>
       </div>
