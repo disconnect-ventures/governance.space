5e135f6cead83d2f84c6a378bb12bfb94d08affd
lucasnbarros <lucasnbarros@gmail.com>
2025-03-11 16:18:25 -0300

    feat: add drep status pie chart to analytics page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,7 @@
-export const PUBLIC_APP_DOMAIN =
-  process.env.NEXT_PUBLIC_APP_DOMAIN || "https://governancespace.com";
+export const PUBLIC_APP_DOMAIN = process.env.NEXT_PUBLIC_APP_DOMAIN || "https://governancespace.com";
 
 export const PDF_API_URL = process.env.API_PDF_BASE_URL || "";
-export const API_BASE_URL = process.env.API_BASE_URL || "";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
 export const METADATA_BASE_URL = process.env.METADATA_BASE_URL || "";
 
 export const FIRST_EPOCH_START = 1506203091000; //September 23, 2017, 21:44:51 UTC
