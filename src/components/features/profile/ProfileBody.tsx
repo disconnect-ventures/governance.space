17b24dc8d9bb4661a04461c3654eefd3f7b7f5ab
Danilo Arantes F. <dfilho@purdue.edu>
2025-03-28 14:37:17 -0300

    feat: Display `notProvided` message for empty drep info

--- src/components/features/profile/ProfileBody.tsx
+++ src/components/features/profile/ProfileBody.tsx
@@ -1,4 +1,3 @@
-// https://www.figma.com/design/bfEklPIeZVRf0P6xC6f1e7/Governance-Space?node-id=50-19626&t=GGJEhGlKd8rVords-4
 import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import { DRep } from "~/lib/dreps";
 import CopyToClipboard from "../CopyToClipboard";
@@ -12,42 +11,54 @@ type ProfileBodyProps = {
 };
 
 export function ProfileBody({ drep, translations }: ProfileBodyProps) {
+  const infoSections = [
+    [translations.objectives, drep.objectives],
+    [translations.motivations, drep.motivations],
+    [translations.qualifications, drep.qualifications],
+    [
+      translations.paymentAddress,
+      drep.paymentAddress ? (
+        <span key={translations.paymentAddress} className="flex gap-2">
+          <span className="text-ellipsis overflow-hidden">
+            {drep.paymentAddress}
+          </span>
+          {drep.paymentAddress && (
+            <CopyToClipboard value={drep.paymentAddress}></CopyToClipboard>
+          )}
+        </span>
+      ) : null,
+    ],
+    [translations.metadataHash, drep.metadataHash],
+    [
+      translations.metadataUrl,
+      drep.url ? (
+        <Link
+          key={translations.metadataUrl}
+          href={drep.url}
+          className="flex gap-2 text-primary hover:underline"
+          target="_blank"
+        >
+          <span className="overflow-hidden text-ellipsis">{drep.url}</span>
+          <LinkIcon className="h-4 inline pl-2"></LinkIcon>
+        </Link>
+      ) : null,
+    ],
+  ];
+
   return (
     <Card>
       <CardHeader></CardHeader>
       <CardContent className="flex flex-col gap-8">
-        {[
-          [translations.objectives, drep.objectives],
-          [translations.motivations, drep.motivations],
-          [translations.qualifications, drep.qualifications],
-          [
-            translations.paymentAddress,
-            <span key={translations.paymentAddress} className="flex gap-2">
-              <span className="text-ellipsis overflow-hidden">
-                {drep.paymentAddress}
-              </span>
-              <CopyToClipboard
-                value={drep.paymentAddress ?? ""}
-              ></CopyToClipboard>
-            </span>,
-          ],
-          [translations.metadataHash, drep.metadataHash],
-          [
-            translations.metadataUrl,
-            <Link
-              key={translations.metadataUrl}
-              href={drep.url}
-              className="flex gap-2 text-primary hover:underline"
-              target="_blank"
-            >
-              <span className="overflow-hidden text-ellipsis">{drep.url}</span>
-              <LinkIcon className="h-4 inline pl-2"></LinkIcon>
-            </Link>,
-          ],
-        ].map(([label, text], index) => (
+        {infoSections.map(([label, text], index) => (
           <div className="space-y-2 w-full" key={index}>
             <h3 className="font-semibold">{label}</h3>
-            <p className="text-ellipsis overflow-hidden">{text}</p>
+            <p className="text-ellipsis overflow-hidden">
+              {text ?? (
+                <span className="text-muted-foreground">
+                  {translations.notProvided}
+                </span>
+              )}
+            </p>
           </div>
         ))}
       </CardContent>
