71ac8fd031e9bcf572fd0c6f3bd7d105045e7bdd
lucasnbarros <lucasnbarros@gmail.com>
2025-04-03 15:58:16 -0300

    feat: translate Breadcrumbs

--- src/components/layout/Breadcrumbs.tsx
+++ src/components/layout/Breadcrumbs.tsx
@@ -1,54 +1,67 @@
 "use client";
-import React from "react";
+import React, { useMemo } from "react";
 import Link from "~/components/features/Link";
 import { usePathname } from "next/navigation";
 import { ChevronRight } from "lucide-react";
 import { i18n } from "~/config/i18n";
+import { Dictionary } from "~/config/dictionaries";
 
-interface BreadcrumbItem {
-  label: string;
-  href: string;
-}
+type BreadcrumbsProps = {
+  translations: Dictionary["breadcrumbs"] & Record<string, string | undefined>;
+};
 
-export const Breadcrumbs = () => {
+export const Breadcrumbs = ({ translations }: BreadcrumbsProps) => {
   const pathname = usePathname();
+
+  const breadcrumbs = useMemo(() => {
+    const homeBreadcrumb = {
+      label: translations.home,
+      href: "/",
+    };
+
+    if (pathname === "/") return [homeBreadcrumb];
+
+    const segments = pathname
+      .split("/")
+      .filter(
+        (segment) =>
+          segment && !i18n.locales.some((locale) => locale.key === segment)
+      );
+
+    const pathBreadcrumbs = segments.map((segment, index) => {
+      const href = `/${segments.slice(0, index + 1).join("/")}`;
+
+      const camelCaseKey = segment.replace(/-([a-z])/g, (_, char) =>
+        char.toUpperCase()
+      );
+
+      const label = translations[camelCaseKey]
+        ? translations[camelCaseKey]
+        : segment
+            .split("-")
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(" ");
+
+      return { label, href };
+    });
+
+    return [homeBreadcrumb, ...pathBreadcrumbs];
+  }, [pathname, translations]);
 
   if (pathname === "/") {
     return null;
   }
 
-  const generateBreadcrumbs = (): BreadcrumbItem[] => {
-    const paths = pathname
-      .split("/")
-      .filter((p) => Boolean(p) && !i18n.locales.some((l) => l.key === p));
-
-    return paths.map((path, index) => {
-      const href = "/" + paths.slice(0, index + 1).join("/");
-      const label = path
-        .split("-")
-        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(" ");
-      return { label, href };
-    });
-  };
-
-  const breadcrumbs = generateBreadcrumbs();
-
   return (
     <nav className="flex items-center space-x-1 text-sm text-muted-foreground mb-4">
-      <Link href="/" className="flex items-center hover:text-foreground">
-        <span>Home</span>
-      </Link>
       {breadcrumbs.map((breadcrumb, index) => (
         <React.Fragment key={breadcrumb.href}>
-          <ChevronRight className="w-4 h-4 text-muted-foreground" />
+          {index > 0 && (
+            <ChevronRight className="w-4 h-4 text-muted-foreground" />
+          )}
           <Link
             href={breadcrumb.href}
-            className={`text-ellipsis overflow-hidden hover:text-foreground ${
-              index === breadcrumbs.length - 1
-                ? "font-medium text-foreground"
-                : ""
-            }`}
+            className={`text-ellipsis overflow-hidden hover:text-foreground ${index === breadcrumbs.length - 1 ? "font-medium text-foreground" : ""}`}
           >
             {breadcrumb.label}
           </Link>
