bf3d89ee8c2bdd13ef88d83f528d4cb0cfbf343c
Danilo Arantes F. <dfilho@purdue.edu>
2025-04-14 17:33:18 -0300

    feat: Add loading indicator when loading more comments

--- src/app/[lang]/proposals/[proposal]/page.tsx
+++ src/app/[lang]/proposals/[proposal]/page.tsx
@@ -93,7 +93,6 @@ export default async function ProposalDetailsPage({
     return notFound();
   }
 
-  // TODO: Add pagination params
   const proposalComments = getProposalComments({
     proposalId: proposalId.toString(),
   });
