a3ea71eae677881d7a8c52f1e94a44269901f6a0
lucasnbarros <lucasnbarros@gmail.com>
2025-04-17 11:23:32 -0300

    feat: translate ProposalTimeline component

--- src/app/[lang]/proposals/[proposal]/page.tsx
+++ src/app/[lang]/proposals/[proposal]/page.tsx
@@ -140,6 +140,7 @@ export default async function ProposalDetailsPage({
                   createdEpoch={createdEpoch}
                   updateDate={updatedAt}
                   updateEpoch={updatedEpoch}
+                  translations={dictionary}
                 />
               </div>
             </div>
