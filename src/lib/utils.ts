8f44a9ec992475cd6651135b1c8df87aebd6e22c
lucasnbarros <lucasnbarros@gmail.com>
2025-04-24 12:12:17 -0300

    fix: correct toCamelCase function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -30,8 +30,8 @@ export function toCamelCase(input: string): string {
     .split(" ")
     .map((word, index) =>
       index === 0
-        ? word.toLowerCase()
-        : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        ? word.charAt(0).toLowerCase() + word.slice(1)
+        : word.charAt(0).toUpperCase() + word.slice(1)
     )
     .join("");
 }
